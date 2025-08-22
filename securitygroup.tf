aa347b2f2b5de71824a56db6bfe4e6bae9eec77c
mosesmu <mosesmuwanguzi1990@gmail.com>
2025-08-22 02:06:53 +0200

    Add fix

--- securitygroup.tf
+++ securitygroup.tf
@@ -27,19 +27,19 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.WebSG.id
   cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" 
+  ip_protocol       = "-1"
 }
 
 
 
 
 resource "aws_security_group" "ALBSG" {
-  name        = "allow_vm_access"
-  description = "Allow VM Access"
+  name        = "allow_alb_access"
+  description = "Allow application load balancer Access"
   vpc_id      = aws_vpc.moses-vpc.id
 
   tags = {
-    Name = "allow_vm_access"
+    Name = "allow_alb_access"
   }
 }
 
@@ -53,6 +53,6 @@ resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4_applb" {
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4_applb" {
   security_group_id = aws_security_group.ALBSG.id
   cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" 
+  ip_protocol       = "-1"
 }
 
